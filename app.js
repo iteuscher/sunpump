fa7ea498e8efa13afcaf1aa063ba12bf16d7d204
iteuscher <iteuscher@headroyce.org>
2018-05-17 14:52:11 -0700

    fixed

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var io = require('socket.io')(http);
 
 let totalRaised = 0;
 
-http.listen(3000, function() {
-    console.log('listening on *:3000');
+http.listen(3001, function() {
+    console.log('listening on *:3001');
  });
 
  io.on('connection', function(socket) {
