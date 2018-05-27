5c2e5698eb8c212f5164ac6d7757ca8a34ea5aea
Isaac Teuscher <isaacteuscher@gmail.com>
2018-05-27 15:03:12 -0700

    Reworked Donate Page (icons, map, order) and confirmation email

--- assets/js/plugins/jquery.easing.js
+++ assets/js/plugins/jquery.easing.js
@@ -22,7 +22,7 @@ $('a[href*="#"]')
                 event.preventDefault();
                 $('html, body').animate({
                     scrollTop: target.offset().top
-                }, 1000, function () {
+                }, 1200, function () {
                     // Callback after animation
                     // Must change focus!
                     var $target = $(target);
